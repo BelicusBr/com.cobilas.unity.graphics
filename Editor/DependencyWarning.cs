cd7bd4764b2cd3b134f79ce370d136313f48e3cb
BelicusBr <X70r@outlook.com>
2022-07-22 01:13:21 -0300

    Revert "Revert "#Update-vs1.0.0-b0.36""
    This reverts commit 54406d5111787d2ef6e84b79682c1c8bc6853c5f.

---
+++ Editor/DependencyWarning.cs
@@ -0,0 +1,9 @@
+﻿#if !COBILAS_CORE
+#error repositorio https://github.com/BelicusBr/com.cobilas.cobilascore.git#v1.0.0 faltando!
+#endif
+#if !COBILAS_MONO
+#error repositorio https://github.com/BelicusBr/com.cobilas.unity.mono.git#v1.0.0 faltando!
+#endif
+#if !COBILAS_EDITOR_UTILITY
+#error repositorio https://github.com/BelicusBr/com.cobilas.unity.editor.utility.git#v1.0.0 faltando!
+#endif
