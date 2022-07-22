0a412989a16f62a473a5f6ddbd0745a8e5e557ee
BelicusBr <X70r@outlook.com>
2022-07-22 00:03:54 -0300

    #Update-vs1.0.0-b0.36

---
+++ Test/Runtime/DependencyWarning.cs
@@ -0,0 +1,12 @@
+﻿#if !COBILAS_CORE
+#error repositorio https://github.com/BelicusBr/com.cobilas.cobilascore.git#v1.0.0 faltando!
+#endif
+#if !COBILAS_MONO
+#error repositorio https://github.com/BelicusBr/com.cobilas.unity.mono.git#v1.0.0 faltando!
+#endif
+#if !COBILAS_EXTENSIONS
+#error repositorio https://github.com/BelicusBr/com.cobilas.unity.extensions.git#v1.0.0 faltando!
+#endif
+#if !COBILAS_UTILITY
+#error repositorio https://github.com/BelicusBr/com.cobilas.unity.utility.git#v1.0.0 faltando!
+#endif
