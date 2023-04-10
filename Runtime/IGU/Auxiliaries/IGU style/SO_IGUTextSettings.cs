a28884fafd832be9f3031686e4825edae2286f42
BelicusBr <X70r@outlook.com>
2023-04-09 21:22:50 -0300

    #Update-vs2.1.0-rc2

---
+++ Runtime/IGU/Auxiliaries/IGU style/SO_IGUTextSettings.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Cobilas.Unity.Graphics.IGU {
+    [CreateAssetMenu(fileName ="IGUText settings", menuName = "IGUSkin/IGUTextSettings")]
+    public class SO_IGUTextSettings : ScriptableObject {
+        [SerializeField] private string settingsName;
+        [SerializeField] private IGUTextSettings settings;
+
+        public string SettingsName => settingsName;
+        public IGUTextSettings Settings => settings;
+
+        private void Awake()
+        {
+            settingsName = string.Empty;
+            settings = new IGUTextSettings();
+        }
+    }
+}
