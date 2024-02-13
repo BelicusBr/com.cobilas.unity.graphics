c390a3133d91c0e1802b06d93dd89099659562b6
BelicusBr <x70r@outlook.com>
2024-02-13 01:15:45 -0300

    #3.5.5-rc1

---
+++ Test/Editor/Cobilas.Unity.Test.Editor.Graphics.asmdef.nowar.cs
@@ -0,0 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
+
+#if !UNITY_EDITOR
+
+[assembly: SuppressMessage("", "IDE0076", Scope = "module")]
+
+#endif
+
+[assembly: SuppressMessage("", "IDE1006", Scope = "module")]
+[assembly: SuppressMessage("", "IDE0028", Scope = "module")]
+[assembly: SuppressMessage("", "IDE0017", Scope = "module")]
